d9875bd782b6c57f1b86d38ab4b0afca1255df5f
Daniil Kolesnykov <35698265+Hanzxzxz@users.noreply.github.com>
2018-04-20 15:28:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // todo: imort encoding function from the separate JS module
-import { caesar } from 'caesar';
+import { caesar } from './caesar';
 
 let input = document.querySelector('.clear-input');
 let output = document.querySelector('.encoded-output');
