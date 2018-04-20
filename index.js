76d9c15157733f19f49080db25ae185c801c11bf
Daniil <kolesnikotti@gmail.com>
2018-04-20 12:43:05 +0300

    changes

---
+++ index.js
@@ -0,0 +1,11 @@
+// todo: imort encoding function from the separate JS module
+import { caesar } from 'caesar';
+
+let input = document.querySelector('.clear-input');
+let output = document.querySelector('.encoded-output');
+
+input.addEventListener('input', onInput);
+function onInput(event) {
+  // todo: use imported function to encode the input
+  output.value = event.target.value.caesar();
+}
